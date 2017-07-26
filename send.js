facbe427937845121cdfd840b8a66c5179143566
Gabriel Lundgren <gabriel.lundgren@eldorado.org.br>
2017-07-26 15:44:01 -0300

    first commit

---
+++ send.js
@@ -0,0 +1,19 @@
+const amqp = require("amqplib/callback_api");
+
+const config = require("./config");
+
+let obj = {
+  test: "lala",
+  name: "gaga"
+};
+
+amqp.connect(config.AMPQ_URL, (err, conn) => {
+  conn.createChannel((err, ch) => {
+    let q = config.QUEUE_NAME;
+
+    ch.assertQueue(q);
+    // Send a single message with the content given as a buffer to the specific queue named
+    ch.sendToQueue(q, Buffer.from(JSON.stringify(obj)));
+    console.log("%s sent", obj.name);
+  });
+});
