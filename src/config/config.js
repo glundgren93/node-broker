4f58e4c35c0470dd3d9e98da79998dc5204c3330
Gabriel Lundgren <glundgren93@gmail.com>
2017-07-30 14:07:17 -0300

    creating api url constanst

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 module.exports = {
-  AMPQ_URL: "amqp://localhost",
-  EXCHANGE_NAME: "testExchange"
+  AMQP_URL: "amqp://localhost",
+  EXCHANGE_NAME: "testExchange",
+  API_REQUEST_URL: `https://newsapi.org/v1/articles?source=the-new-york-times&sortBy=top&apiKey=${process.env.API_KEY}`
 };
